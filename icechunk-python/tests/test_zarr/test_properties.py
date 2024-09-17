707bd6a2df581756a55ee7521a14247dd70206a6
Deepak Cherian <deepak@earthmover.io>
2024-09-17 15:09:54 -0600

    temp

--- icechunk-python/tests/test_zarr/test_properties.py
+++ icechunk-python/tests/test_zarr/test_properties.py
@@ -15,9 +15,11 @@ icechunk_stores = st.builds(icechunk.IcechunkStore.from_json,
                             config=st.just({"storage": {"type": "in_memory"}, "dataset": {}}),
                             mode=st.just("w")).map(lambda x: sync(x))
 
+from hypothesis import settings
+@settings(report_multiple_bugs=False)
 @given(st.data())
 def test_roundtrip(data: st.DataObject) -> None:
-    nparray = data.draw(np_arrays)
+    nparray = data.draw(np_arrays.filter(lambda x: x.dtype.kind != "M" and ">" not in x.dtype.str))
     zarray = data.draw(arrays(stores=icechunk_stores,
                               arrays=st.just(nparray)))
     assert_array_equal(nparray, zarray[:])
