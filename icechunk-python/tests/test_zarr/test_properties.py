2bb8fe610153f66170a23f47cdac50a7beb1ba4f
Deepak Cherian <deepak@earthmover.io>
2024-09-17 10:34:47 -0600

    Wire up Zarr property tests

---
+++ icechunk-python/tests/test_zarr/test_properties.py
@@ -0,0 +1,26 @@
+import numpy as np
+from functools import partial
+import pytest
+from numpy.testing import assert_array_equal
+from zarr.core.sync import sync
+pytest.importorskip("hypothesis")
+
+import hypothesis.extra.numpy as npst  # noqa
+import hypothesis.strategies as st  # noqa
+from hypothesis import given, settings  # noqa
+from zarr.testing.strategies import arrays, np_arrays, basic_indices  # noqa
+import icechunk
+
+def sync_from_json(func, **kwargs):
+    return sync(func(**kwargs))
+
+icechunk_stores = st.builds(partial(sync_from_json, icechunk.IcechunkStore.from_json),
+                            config=st.just({"storage": {"type": "in_memory"}, "dataset": {}}),
+                            mode=st.just("w"),)
+
+@given(st.data())
+def test_roundtrip(data: st.DataObject) -> None:
+    nparray = data.draw(np_arrays)
+    zarray = data.draw(arrays(stores=icechunk_stores,
+                              arrays=st.just(nparray)))
+    assert_array_equal(nparray, zarray[:])
