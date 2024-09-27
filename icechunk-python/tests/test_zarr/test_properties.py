5cba96f54b91335df51b05336d773f7819a47d6f
Deepak Cherian <deepak@earthmover.io>
2024-09-27 11:04:51 -0600

    silence ruff

--- icechunk-python/tests/test_zarr/test_properties.py
+++ icechunk-python/tests/test_zarr/test_properties.py
@@ -3,6 +3,7 @@ from functools import partial
 import pytest
 from numpy.testing import assert_array_equal
 from zarr.core.sync import sync
+
 pytest.importorskip("hypothesis")
 
 import hypothesis.extra.numpy as npst  # noqa
@@ -11,16 +12,18 @@ from hypothesis import given, settings  # noqa
 from zarr.testing.strategies import arrays, np_arrays, basic_indices  # noqa
 from icechunk import IcechunkStore, Storage
 
-icechunk_stores = st.builds(IcechunkStore.open,
-                            storage=st.builds(Storage.memory),
-                            mode=st.just("w")).map(lambda x: sync(x))
+icechunk_stores = st.builds(
+    IcechunkStore.open, storage=st.builds(Storage.memory), mode=st.just("w")
+).map(lambda x: sync(x))
 
 from hypothesis import settings
+
 
 @settings(report_multiple_bugs=False)
 @given(st.data())
 def test_roundtrip(data: st.DataObject) -> None:
-    nparray = data.draw(np_arrays.filter(lambda x: x.dtype.kind != "M" and ">" not in x.dtype.str))
-    zarray = data.draw(arrays(stores=icechunk_stores,
-                              arrays=st.just(nparray)))
+    nparray = data.draw(
+        np_arrays.filter(lambda x: x.dtype.kind != "M" and ">" not in x.dtype.str)
+    )
+    zarray = data.draw(arrays(stores=icechunk_stores, arrays=st.just(nparray)))
     assert_array_equal(nparray, zarray[:])
