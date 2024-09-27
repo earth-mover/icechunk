eddeb1e1ce4999461f5147925f064ca61797b291
Matthew Iannucci <mpiannucci@gmail.com>
2024-09-27 12:02:39 -0400

    Add ability to set virtual chunks from python [EAR-1333] (#114)
    * Add virtual ref python bindings without test case
    
    * Add virtual ref test
    
    * Further flesh out testing
    
    * Update doc strings
    
    * Make params keyword args

--- icechunk-python/python/icechunk/__init__.py
+++ icechunk-python/python/icechunk/__init__.py
@@ -331,6 +331,24 @@ class IcechunkStore(Store, SyncMixin):
         """
         return await self._store.set(key, value.to_bytes())
 
+    async def set_virtual_ref(
+        self, key: str, location: str, *, offset: int, length: int
+    ) -> None:
+        """Store a virtual reference to a chunk.
+
+        Parameters
+        ----------
+        key : str
+            The chunk to store the reference under. This is the fully qualified zarr key eg: 'array/c/0/0/0'
+        location : str
+            The location of the chunk in storage. This is absolute path to the chunk in storage eg: 's3://bucket/path/to/file.nc'
+        offset : int
+            The offset in bytes from the start of the file location in storage the chunk starts at
+        length : int
+            The length of the chunk in bytes, measured from the given offset
+        """
+        return await self._store.set_virtual_ref(key, location, offset, length)
+
     async def delete(self, key: str) -> None:
         """Remove a key from the store
 
