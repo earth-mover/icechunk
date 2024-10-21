061eeb6cf364977bb2ef9e90aa8fffc17a05f26f
Deepak Cherian <deepak@earthmover.io>
2024-10-21 15:38:08 -0600

    updates

--- icechunk-python/tests/test_zarr/test_properties.py
+++ icechunk-python/tests/test_zarr/test_properties.py
@@ -9,21 +9,23 @@ pytest.importorskip("hypothesis")
 import hypothesis.extra.numpy as npst  # noqa
 import hypothesis.strategies as st  # noqa
 from hypothesis import given, settings  # noqa
-from zarr.testing.strategies import arrays, np_arrays, basic_indices  # noqa
-from icechunk import IcechunkStore, Storage
+from zarr.testing.strategies import arrays, numpy_arrays, basic_indices  # noqa
+from icechunk import IcechunkStore, StorageConfig
 
 icechunk_stores = st.builds(
-    IcechunkStore.open, storage=st.builds(Storage.memory), mode=st.just("w")
+    IcechunkStore.open,
+    storage=st.builds(StorageConfig.memory),
+    prefix=st.just("prefix"),
+    mode=st.just("w"),
 ).map(lambda x: sync(x))
 
 from hypothesis import settings
 
 
 @settings(report_multiple_bugs=False)
-@given(st.data())
-def test_roundtrip(data: st.DataObject) -> None:
-    nparray = data.draw(
-        np_arrays.filter(lambda x: x.dtype.kind != "M" and ">" not in x.dtype.str)
+@given(data=st.data(), nparray=numpy_arrays(zarr_formats=st.just(3)))
+def test_roundtrip(data: st.DataObject, nparray) -> None:
+    zarray = data.draw(
+        arrays(stores=icechunk_stores, arrays=st.just(nparray), zarr_formats=st.just(3))
     )
-    zarray = data.draw(arrays(stores=icechunk_stores, arrays=st.just(nparray)))
     assert_array_equal(nparray, zarray[:])
