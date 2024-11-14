26e1836e4b5d3abe82d8e2b0f286273d24c5735d
Deepak Cherian <deepak@earthmover.io>
2024-11-14 16:16:41 -0700

    try again

--- icechunk-python/tests/test_dask.py
+++ icechunk-python/tests/test_dask.py
@@ -3,19 +3,18 @@ import pytest
 pytest.importorskip("distributed")
 from distributed import Client
 from distributed.utils_test import (  # noqa: F401
-    cleanup,
-    cluster,
+    client,
     loop,
     loop_in_thread,
+    cleanup,
+    cluster_fixture,
 )
 from xarray.testing import assert_identical
 
 from .test_xarray import create_test_data, roundtrip
 
 
-def test_xarray_to_icechunk_distributed(loop):
-    with cluster() as (s, [a, b]):
-        with Client(s["address"], loop=loop):
-            ds = create_test_data().chunk(dim1=3, dim2=4)
-            with roundtrip(ds) as actual:
-                assert_identical(actual, ds)
+def test_xarray_to_icechunk_distributed(client):
+    ds = create_test_data().chunk(dim1=3, dim2=4)
+    with roundtrip(ds) as actual:
+        assert_identical(actual, ds)
