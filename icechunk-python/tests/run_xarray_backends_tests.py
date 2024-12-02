51d131000789065393ce9a485bc0d224e6235442
Deepak Cherian <deepak@earthmover.io>
2024-12-02 13:38:03 -0700

    Add fs,mem,minio testing

--- icechunk-python/tests/run_xarray_backends_tests.py
+++ icechunk-python/tests/run_xarray_backends_tests.py
@@ -1,11 +1,12 @@
 import contextlib
 import os
 import tempfile
+import time
 
 import pytest
 
 import zarr
-from icechunk import IcechunkStore, StorageConfig
+from icechunk import IcechunkStore, S3Credentials, StorageConfig
 from xarray.tests.test_backends import (
     ZarrBase,
     default_zarr_version,  # noqa: F401; needed otherwise not discovered
@@ -16,14 +17,7 @@ from xarray.tests.test_backends import (
     not os.environ.get("ICECHUNK_XARRAY_BACKENDS_TESTS", None),
     reason="skipping Xarray backends tests",
 )
-class TestIcechunkStore(ZarrBase):
-    @contextlib.contextmanager
-    def create_zarr_target(self):
-        if zarr.config.config["default_zarr_version"] == 2:
-            pytest.skip("v2 not supported")
-        with tempfile.TemporaryDirectory() as tmpdir:
-            yield IcechunkStore.create(StorageConfig.filesystem(tmpdir))
-
+class IcechunkStoreBase(ZarrBase):
     @pytest.mark.parametrize("consolidated", [False, True, None])
     @pytest.mark.parametrize("compute", [False, True])
     @pytest.mark.parametrize("use_dask", [False, True])
@@ -38,3 +32,42 @@ class TestIcechunkStore(ZarrBase):
         if consolidated is not False:
             pytest.skip("consolidated not supported.")
         super().test_roundtrip_consolidated(consolidated)
+
+
+class TestIcechunkStoreFilesystem(IcechunkStoreBase):
+    @contextlib.contextmanager
+    def create_zarr_target(self):
+        if zarr.config.config["default_zarr_version"] == 2:
+            pytest.skip("v2 not supported")
+        with tempfile.TemporaryDirectory() as tmpdir:
+            yield IcechunkStore.create(StorageConfig.filesystem(tmpdir))
+
+
+class TestIcechunkStoreMemory(IcechunkStoreBase):
+    @contextlib.contextmanager
+    def create_zarr_target(self):
+        if zarr.config.config["default_zarr_version"] == 2:
+            pytest.skip("v2 not supported")
+        yield IcechunkStore.create(StorageConfig.memory())
+
+
+class TestIcechunkStoreMinio(IcechunkStoreBase):
+    @contextlib.contextmanager
+    def create_zarr_target(self):
+        if zarr.config.config["default_zarr_version"] == 2:
+            pytest.skip("v2 not supported")
+        storage_config = {
+            "bucket": "testbucket",
+            "prefix": "python-xarray-test__" + str(time.time()),
+            "endpoint_url": "http://localhost:9000",
+            "region": "us-east-1",
+            "allow_http": True,
+        }
+        storage_config = StorageConfig.s3_from_config(
+            **storage_config,
+            credentials=S3Credentials(
+                access_key_id="minio123", secret_access_key="minio123"
+            ),
+        )
+        store = IcechunkStore.create(storage=storage_config, read_only=False)
+        yield store
