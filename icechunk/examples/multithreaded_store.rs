0e7da0a22d5b0ed0c9c00b7b6679bcb60f110017
Matthew Iannucci <matthew@earthmover.io>
2024-12-11 11:22:36 -0500

    tests compiling and failing

--- icechunk/examples/multithreaded_store.rs
+++ icechunk/examples/multithreaded_store.rs
@@ -2,28 +2,28 @@ use std::{ops::Range, sync::Arc, time::Duration};
 
 use bytes::Bytes;
 use futures::StreamExt;
-use icechunk::{format::ByteRange, Store};
+use icechunk::{
+    format::ByteRange, store::StoreOptions, ObjectStorage, Repository, RepositoryConfig,
+    Store,
+};
 use tokio::{sync::RwLock, task::JoinSet, time::sleep};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let store_config_json = br#"
-        {"storage":
-            {"type": "in_memory"},
-            "repository": {
-                "previous_version": null,
-                "inline_chunk_threshold_bytes":128
-            }}
-    "#;
-
-    let store =
-        Store::from_json(store_config_json, icechunk::zarr::AccessMode::ReadWrite)
-            .await?;
-    let store = Arc::new(RwLock::new(store));
+    let storage = Arc::new(ObjectStorage::new_in_memory_store(None)?);
+    let repo = Repository::create(
+        RepositoryConfig {
+            inline_chunk_threshold_bytes: 128,
+            unsafe_overwrite_refs: true,
+        },
+        storage,
+        None,
+    )
+    .await?;
+    let ds = Arc::new(RwLock::new(repo.writeable_session("main").await?));
+    let store = Store::from_session(Arc::clone(&ds), StoreOptions::default(), false);
 
     store
-        .write()
-        .await
         .set(
             "zarr.json",
             Bytes::copy_from_slice(br#"{"zarr_format":3, "node_type":"group"}"#),
@@ -31,19 +31,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
 
     let zarr_meta = Bytes::copy_from_slice(br#"{"zarr_format":3,"node_type":"array","attributes":{"foo":42},"shape":[10000],"data_type":"float32","chunk_grid":{"name":"regular","configuration":{"chunk_shape":[1]}},"chunk_key_encoding":{"name":"default","configuration":{"separator":"/"}},"fill_value":0.0,"codecs":[{"name":"mycodec","configuration":{"foo":42}}],"storage_transformers":[{"name":"mytransformer","configuration":{"bar":43}}],"dimension_names":["x"]}"#);
-    store.write().await.set("array/zarr.json", zarr_meta.clone()).await?;
+    store.set("array/zarr.json", zarr_meta.clone()).await?;
 
     // We start 100 async tasks that loop until they successfully read a specific chunk.
     // Initially this will loop, because no chunks are written.
     let mut set = JoinSet::new();
     for i in 500..600 {
-        let store = Arc::clone(&store);
+        let store = Store::from_session(Arc::clone(&ds), StoreOptions::default(), false);
         set.spawn(async move {
             let mut attempts = 0u64;
             loop {
                 if let Ok(value) = store
-                    .read()
-                    .await
                     .get(format!("array/c/{i}").as_str(), &ByteRange::ALL)
                     .await
                 {
@@ -58,12 +56,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         });
     }
 
-    async fn writer(name: &str, range: Range<u64>, store: Arc<RwLock<Store>>) {
+    async fn writer(name: &str, range: Range<u64>, store: &Store) {
         println!("Starting writer {name}.");
         for i in range {
             if let Err(err) = store
-                .write()
-                .await
                 .set(
                     format!("array/c/{i}").as_str(),
                     Bytes::from(format!("Writer {name} wrote {i}")),
@@ -79,18 +75,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     // We start two tasks that write all the needed chunks, sleeping between writes.
-    let store1 = Arc::clone(&store);
-    let store2 = Arc::clone(&store);
+    let store1 = Store::from_session(Arc::clone(&ds), StoreOptions::default(), false);
+    let store2 = Store::from_session(Arc::clone(&ds), StoreOptions::default(), false);
 
-    let writer1 = tokio::spawn(async move { writer("1", 500..550, store1).await });
-    let writer2 = tokio::spawn(async move { writer("2", 550..600, store2).await });
+    let writer1 = tokio::spawn(async move { writer("1", 500..550, &store1).await });
+    let writer2 = tokio::spawn(async move { writer("2", 550..600, &store2).await });
 
     // we join all tasks, program will stop once all chunks are successfully read
     writer1.await?;
     writer2.await?;
     while (set.join_next().await).is_some() {}
 
-    let all_keys = store.read().await.list().await?.count().await;
+    let all_keys = store.list().await?.count().await;
     println!("Found {all_keys} keys in the store: 100 chunks + 1 root group metadata + 1 array metadata");
 
     Ok(())
