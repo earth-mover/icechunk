039b67b049022c534d049d229f9c64e31d5c0535
Sebastian Galkin <code@amisdelabc.com>
2024-12-26 14:55:41 -0300

    Bring 'test_can_read_old' test back to life

--- icechunk-python/tests/conftest.py
+++ icechunk-python/tests/conftest.py
@@ -1,5 +1,6 @@
 from typing import Literal
 
+import boto3
 import pytest
 
 from icechunk import ObjectStoreConfig, Repository, Storage
@@ -21,3 +22,32 @@ def repo(request: pytest.FixtureRequest, tmpdir: str) -> tuple[Repository, str]:
     param = request.param
     repo = parse_repo(param, tmpdir)
     return repo, tmpdir
+
+
+minio_client = None
+
+
+def get_minio_client():
+    global minio_client
+    if minio_client is None:
+        minio_client = boto3.client(
+            "s3",
+            endpoint_url="http://localhost:9000",
+            use_ssl=False,
+            aws_access_key_id="minio123",
+            aws_secret_access_key="minio123",
+        )
+    return minio_client
+
+
+def write_chunks_to_minio(
+    chunks: list[tuple[str, bytes]], bucket: str = "testbucket"
+) -> list[str]:
+    """Write chunks to local minio returning their etags"""
+    s3 = get_minio_client()
+    etags = []
+    for key, data in chunks:
+        etag = s3.put_object(Bucket=bucket, Key=key, Body=data)["ETag"]
+        etags.append(etag)
+
+    return etags
