4e9ad543e607d326df1cb1ddee5ad065b9ad67f1
Deepak Cherian <deepak@earthmover.io>
2025-01-27 16:53:08 -0700

    Minimal support for compute kwarg

--- icechunk-python/tests/test_dask.py
+++ icechunk-python/tests/test_dask.py
@@ -5,18 +5,14 @@ pytest.importorskip("distributed")
 
 import dask
 import distributed
-import xarray as xr
-from icechunk import Repository, local_filesystem_storage
-from icechunk.xarray import to_icechunk
 from tests.test_xarray import create_test_data, roundtrip
 from xarray.testing import assert_identical
 
 
-@pytest.mark.parametrize("compute", [False, True])
-def test_distributed(compute: bool) -> None:
+def test_distributed() -> None:
     with distributed.Client():  # type: ignore [no-untyped-call]
         ds = create_test_data().chunk(dim1=3, dim2=4)
-        with roundtrip(ds, compute=compute) as actual:
+        with roundtrip(ds) as actual:
             assert_identical(actual, ds)
 
         # with pytest.raises(ValueError, match="Session cannot be serialized"):
@@ -24,31 +20,10 @@ def test_distributed(compute: bool) -> None:
         #         pass
 
 
-def test_distributed_workflow(tmpdir):
-    repo = Repository.create(
-        storage=local_filesystem_storage(tmpdir),
-    )
-    with distributed.Client():  # type: ignore [no-untyped-call]
-        ds = create_test_data().chunk(dim1=3, dim2=4)
-        session = repo.writable_session("main")
-        future = to_icechunk(ds, session.store, compute=False, mode="w")
-
-        session = repo.writable_session("main")
-        (new_session,) = dask.compute(future)
-        session.merge(new_session)
-        session.commit("foo")
-
-        roundtripped = xr.open_zarr(
-            repo.readonly_session(branch="main").store, consolidated=False
-        )
-        assert_identical(roundtripped, ds)
-
-
-@pytest.mark.parametrize("compute", [False, True])
-def test_threaded(compute: bool) -> None:
+def test_threaded() -> None:
     with dask.config.set(scheduler="threads"):
         ds = create_test_data().chunk(dim1=3, dim2=4)
-        with roundtrip(ds, compute=compute) as actual:
+        with roundtrip(ds) as actual:
             assert_identical(actual, ds)
         # with roundtrip(ds, allow_distributed_write=False) as actual:
         #     assert_identical(actual, ds)
