942f9374d14357e29b329c7ac609087aefba2d26
Sebastian Galkin <code@amisdelabc.com>
2025-02-07 17:02:32 -0300

    Expose spec version to python

--- icechunk-python/src/lib.rs
+++ icechunk-python/src/lib.rs
@@ -24,7 +24,7 @@ use errors::{
     IcechunkError, PyConflictError, PyConflictErrorData, PyRebaseFailedData,
     PyRebaseFailedError,
 };
-use icechunk::initialize_tracing;
+use icechunk::{format::format_constants::SpecVersionBin, initialize_tracing};
 use pyo3::prelude::*;
 use repository::{PyGCSummary, PyRepository, PySnapshotInfo};
 use session::PySession;
@@ -36,6 +36,13 @@ fn initialize_logs() -> PyResult<()> {
         initialize_tracing()
     }
     Ok(())
+}
+
+#[pyfunction]
+/// The spec version that this version of the Icechunk library
+/// uses to write metadata files
+fn spec_version() -> u8 {
+    SpecVersionBin::current() as u8
 }
 
 /// The icechunk Python module implemented in Rust.
@@ -73,6 +80,7 @@ fn _icechunk_python(py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<PyStorageSettings>()?;
     m.add_class::<PyGCSummary>()?;
     m.add_function(wrap_pyfunction!(initialize_logs, m)?)?;
+    m.add_function(wrap_pyfunction!(spec_version, m)?)?;
 
     // Exceptions
     m.add("IcechunkError", py.get_type::<IcechunkError>())?;
