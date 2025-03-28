41fbf5dd32bac0982866b68238d8bdfb2771a3c7
Deepak Cherian <dcherian@users.noreply.github.com>
2025-03-28 15:11:17 -0600

    Remove cache entries during GC (#875)

--- icechunk/src/asset_manager.rs
+++ icechunk/src/asset_manager.rs
@@ -138,6 +138,22 @@ impl AssetManager {
         )
     }
 
+    pub fn remove_cached_snapshot(&self, snapshot_id: &SnapshotId) {
+        self.snapshot_cache.remove(snapshot_id);
+    }
+
+    pub fn remove_cached_manifest(&self, manifest_id: &ManifestId) {
+        self.manifest_cache.remove(manifest_id);
+    }
+
+    pub fn remove_cached_tx_log(&self, snapshot_id: &SnapshotId) {
+        self.transactions_cache.remove(snapshot_id);
+    }
+
+    pub fn clear_chunk_cache(&self) {
+        self.chunk_cache.clear();
+    }
+
     #[instrument(skip(self, manifest))]
     pub async fn write_manifest(&self, manifest: Arc<Manifest>) -> RepositoryResult<u64> {
         let manifest_c = Arc::clone(&manifest);
