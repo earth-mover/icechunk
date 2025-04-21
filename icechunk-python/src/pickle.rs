42b74717e47fe4a886d43c2cfeef29d5b2e5d36f
Matthew Iannucci <matthew@earthmover.io>
2025-04-21 16:49:53 -0400

    Fix lifetimes

--- icechunk-python/src/pickle.rs
+++ icechunk-python/src/pickle.rs
@@ -3,12 +3,12 @@ macro_rules! impl_pickle {
     ($struct_name:ident) => {
         #[pymethods]
         impl $struct_name {
-            pub fn __setstate__<'py>(
+            pub fn __setstate__(
                 &mut self,
-                state: &pyo3::Bound<'py, pyo3::types::PyBytes>,
+                state: &pyo3::Bound<'_, pyo3::types::PyBytes>,
             ) -> pyo3::PyResult<()> {
                 *self = serde_json::from_slice(state.as_bytes()).map_err(|e| {
-                    crate::errors::PyIcechunkStoreError::PickleError(format!(
+                    $crate::errors::PyIcechunkStoreError::PickleError(format!(
                         "Failed to unpickle {}: {}",
                         stringify!($struct_name),
                         e.to_string()
@@ -22,7 +22,7 @@ macro_rules! impl_pickle {
                 py: pyo3::Python<'py>,
             ) -> pyo3::PyResult<pyo3::Bound<'py, pyo3::types::PyBytes>> {
                 let state = serde_json::to_vec(&self).map_err(|e| {
-                    crate::errors::PyIcechunkStoreError::PickleError(format!(
+                    $crate::errors::PyIcechunkStoreError::PickleError(format!(
                         "Failed to pickle {}: {}",
                         stringify!($struct_name),
                         e.to_string()
