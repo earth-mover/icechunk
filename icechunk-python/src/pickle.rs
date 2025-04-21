5d4364792fdc4c5a95086147c7572cbee2224afe
Matthew Iannucci <matthew@earthmover.io>
2025-04-21 16:33:52 -0400

    Forgot to add macro

---
+++ icechunk-python/src/pickle.rs
@@ -0,0 +1,28 @@
+#[macro_export]
+macro_rules! impl_pickle {
+    ($struct_name:ident) => {
+        #[pymethods]
+        impl $struct_name {
+            pub fn __setstate__<'py>(
+                &mut self,
+                state: &pyo3::Bound<'py, pyo3::types::PyBytes>,
+            ) -> pyo3::PyResult<()> {
+                *self = serde_json::from_slice(state.as_bytes()).map_err(|e| {
+                    crate::errors::PyIcechunkStoreError::UnkownError(e.to_string())
+                })?;
+                Ok(())
+            }
+
+            pub fn __getstate__<'py>(
+                &self,
+                py: pyo3::Python<'py>,
+            ) -> pyo3::PyResult<pyo3::Bound<'py, pyo3::types::PyBytes>> {
+                let state = serde_json::to_vec(&self).map_err(|e| {
+                    crate::errors::PyIcechunkStoreError::UnkownError(e.to_string())
+                })?;
+                let bytes = pyo3::types::PyBytes::new(py, &state);
+                Ok(bytes)
+            }
+        }
+    };
+}
