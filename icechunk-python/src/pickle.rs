360f9bafc6deb13f8e5b1d5f23285fd0e0b75df8
Matthew Iannucci <matthew@earthmover.io>
2025-04-22 11:01:18 -0400

    Change internal format to use rmp instead of json

--- icechunk-python/src/pickle.rs
+++ icechunk-python/src/pickle.rs
@@ -1,3 +1,16 @@
+/// macro to implement the necessary methods to pickle and unpickle a python
+/// bound object in pyo3.
+///
+///
+/// Rust structs bound to python are not able to automatically pickle and unpickle
+/// because pyo3 does not implement the `__getstate__` and `__setstate__` methods
+/// for them. This macro implements those methods for the given struct.
+/// See https://github.com/PyO3/pyo3/issues/100 for more information.
+///
+/// For this struct to be applied to a struct, the struct must implement the
+/// `Serialize` and `Deserialize` traits from serde. The format is an internal
+/// implementation detail and is subject to change.
+///
 #[macro_export]
 macro_rules! impl_pickle {
     ($struct_name:ident) => {
@@ -7,7 +20,7 @@ macro_rules! impl_pickle {
                 &mut self,
                 state: &pyo3::Bound<'_, pyo3::types::PyBytes>,
             ) -> pyo3::PyResult<()> {
-                *self = serde_json::from_slice(state.as_bytes()).map_err(|e| {
+                *self = rmp_serde::from_slice(state.as_bytes()).map_err(|e| {
                     $crate::errors::PyIcechunkStoreError::PickleError(format!(
                         "Failed to unpickle {}: {}",
                         stringify!($struct_name),
@@ -21,7 +34,7 @@ macro_rules! impl_pickle {
                 &self,
                 py: pyo3::Python<'py>,
             ) -> pyo3::PyResult<pyo3::Bound<'py, pyo3::types::PyBytes>> {
-                let state = serde_json::to_vec(&self).map_err(|e| {
+                let state = rmp_serde::to_vec(&self).map_err(|e| {
                     $crate::errors::PyIcechunkStoreError::PickleError(format!(
                         "Failed to pickle {}: {}",
                         stringify!($struct_name),
