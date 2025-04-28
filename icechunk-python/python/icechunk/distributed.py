79e8f653c6277697ec963520e1f5c32a8c2e4d72
fjetter <fjetter@users.noreply.github.com>
2025-04-28 16:26:52 +0200

    special path for meta computation

--- icechunk-python/python/icechunk/distributed.py
+++ icechunk-python/python/icechunk/distributed.py
@@ -2,7 +2,9 @@
 from collections.abc import Generator, Iterable
 from typing import Any, cast
 
+import numpy as np
 import zarr
+
 from icechunk import IcechunkStore, Session
 
 __all__ = [
@@ -24,8 +26,13 @@ def _flatten(seq: Iterable[Any], container: type = list) -> Generator[Any, None,
 
 
 def extract_session(
-    zarray: zarr.Array, axis: Any = None, keepdims: Any = None
+    zarray: zarr.Array,
+    axis: Any = None,
+    keepdims: Any = None,
+    computing_meta: bool = False,
 ) -> Session:
+    if computing_meta:
+        return np.array([object()], dtype=object)
     store = cast(IcechunkStore, zarray.store)
     return store.session
 
@@ -34,7 +41,10 @@ def merge_sessions(
     *sessions: Session | list[Session] | list[list[Session]],
     axis: Any = None,
     keepdims: Any = None,
+    computing_meta: bool = False,
 ) -> Session:
+    if computing_meta:
+        return np.array([object()], dtype=object)
     session, *rest = list(_flatten(sessions))
     for other in rest:
         session.merge(other)
