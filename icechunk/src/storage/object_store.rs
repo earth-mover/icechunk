7fd5dc2fa7dcd110e22015113d28aa0bdaec7a2f
Matthew Iannucci <matthew@earthmover.io>
2025-06-04 15:12:54 -0400

    Correctly support anonymous gcp credentials

--- icechunk/src/storage/object_store.rs
+++ icechunk/src/storage/object_store.rs
@@ -932,7 +932,8 @@ impl ObjectStoreBackend for GcsObjectStoreBackend {
                     GcsRefreshableCredentialProvider::new(Arc::clone(fetcher));
                 builder.with_credentials(Arc::new(credential_provider))
             }
-            None | Some(GcsCredentials::FromEnv) | Some(GcsCredentials::Anonymous) => {
+            Some(GcsCredentials::Anonymous) => builder.with_skip_signature(true),
+            None | Some(GcsCredentials::FromEnv) => {
                 GoogleCloudStorageBuilder::from_env()
             }
         };
