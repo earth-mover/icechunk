0bb2bf3384dedef74955ebafd21acb88a22e2ebd
Matthew Iannucci <mpiannucci@gmail.com>
2025-08-03 16:45:47 -0400

    Add react example

---
+++ icechunk-js/examples/react-example/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Vite + React + Icechunk</h1>
+      <p>Hello World</p>
+    </>
+  )
+}
+
+export default App
