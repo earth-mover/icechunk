70016dd9c7901d2642ce70402a1a03122e616987
Matthew Iannucci <mpiannucci@gmail.com>
2026-02-09 14:56:18 -0500

    Gate non wasm code

--- icechunk-js/src/storage.rs
+++ icechunk-js/src/storage.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::sync::Arc;
 
 use icechunk::Storage;
@@ -6,191 +5,206 @@ use napi_derive::napi;
 
 use crate::errors::IntoNapiResult;
 
+#[cfg(not(target_family = "wasm"))]
+use std::collections::HashMap;
+
 #[napi(js_name = "Storage")]
 pub struct JsStorage(pub(crate) Arc<dyn Storage + Send + Sync>);
 
-/// S3 static credentials
-#[napi(object, js_name = "S3StaticCredentials")]
-#[derive(Clone, Debug)]
-pub struct JsS3StaticCredentials {
-    pub access_key_id: String,
-    pub secret_access_key: String,
-    pub session_token: Option<String>,
-}
+#[cfg(not(target_family = "wasm"))]
+mod native {
+    use super::*;
 
-impl From<JsS3StaticCredentials> for icechunk::config::S3StaticCredentials {
-    fn from(creds: JsS3StaticCredentials) -> Self {
-        icechunk::config::S3StaticCredentials {
-            access_key_id: creds.access_key_id,
-            secret_access_key: creds.secret_access_key,
-            session_token: creds.session_token,
-            expires_after: None,
+    /// S3 static credentials
+    #[napi(object, js_name = "S3StaticCredentials")]
+    #[derive(Clone, Debug)]
+    pub struct JsS3StaticCredentials {
+        pub access_key_id: String,
+        pub secret_access_key: String,
+        pub session_token: Option<String>,
+    }
+
+    impl From<JsS3StaticCredentials> for icechunk::config::S3StaticCredentials {
+        fn from(creds: JsS3StaticCredentials) -> Self {
+            icechunk::config::S3StaticCredentials {
+                access_key_id: creds.access_key_id,
+                secret_access_key: creds.secret_access_key,
+                session_token: creds.session_token,
+                expires_after: None,
+            }
         }
     }
-}
 
-/// S3 credentials
-#[napi(js_name = "S3Credentials")]
-pub enum JsS3Credentials {
-    FromEnv,
-    Anonymous,
-    Static(JsS3StaticCredentials),
-}
+    /// S3 credentials
+    #[napi(js_name = "S3Credentials")]
+    pub enum JsS3Credentials {
+        FromEnv,
+        Anonymous,
+        Static(JsS3StaticCredentials),
+    }
 
-impl From<JsS3Credentials> for icechunk::config::S3Credentials {
-    fn from(creds: JsS3Credentials) -> Self {
-        match creds {
-            JsS3Credentials::FromEnv => icechunk::config::S3Credentials::FromEnv,
-            JsS3Credentials::Anonymous => icechunk::config::S3Credentials::Anonymous,
-            JsS3Credentials::Static(c) => {
-                icechunk::config::S3Credentials::Static(c.into())
+    impl From<JsS3Credentials> for icechunk::config::S3Credentials {
+        fn from(creds: JsS3Credentials) -> Self {
+            match creds {
+                JsS3Credentials::FromEnv => icechunk::config::S3Credentials::FromEnv,
+                JsS3Credentials::Anonymous => {
+                    icechunk::config::S3Credentials::Anonymous
+                }
+                JsS3Credentials::Static(c) => {
+                    icechunk::config::S3Credentials::Static(c.into())
+                }
+            }
+        }
+    }
+
+    /// S3 options
+    #[napi(object, js_name = "S3Options")]
+    #[derive(Clone, Debug)]
+    pub struct JsS3Options {
+        pub region: Option<String>,
+        pub endpoint_url: Option<String>,
+        pub allow_http: Option<bool>,
+        pub anonymous: Option<bool>,
+        pub force_path_style: Option<bool>,
+        pub network_stream_timeout_seconds: Option<u32>,
+        pub requester_pays: Option<bool>,
+    }
+
+    impl JsS3Options {
+        #[allow(clippy::too_many_arguments)]
+        pub fn new(
+            region: Option<String>,
+            endpoint_url: Option<String>,
+            allow_http: Option<bool>,
+            anonymous: Option<bool>,
+            force_path_style: Option<bool>,
+            network_stream_timeout_seconds: Option<u32>,
+            requester_pays: Option<bool>,
+        ) -> Self {
+            Self {
+                region,
+                endpoint_url,
+                allow_http,
+                anonymous,
+                force_path_style,
+                network_stream_timeout_seconds,
+                requester_pays,
+            }
+        }
+    }
+
+    impl From<JsS3Options> for icechunk::config::S3Options {
+        fn from(opts: JsS3Options) -> Self {
+            icechunk::config::S3Options {
+                region: opts.region,
+                endpoint_url: opts.endpoint_url,
+                allow_http: opts.allow_http.unwrap_or(false),
+                anonymous: opts.anonymous.unwrap_or(false),
+                force_path_style: opts.force_path_style.unwrap_or(false),
+                network_stream_timeout_seconds: opts.network_stream_timeout_seconds,
+                requester_pays: opts.requester_pays.unwrap_or(false),
+            }
+        }
+    }
+
+    /// GCS static credentials
+    #[napi(js_name = "GcsStaticCredentials")]
+    pub enum JsGcsStaticCredentials {
+        ServiceAccount(String),
+        ServiceAccountKey(String),
+        ApplicationCredentials(String),
+        BearerToken(String),
+    }
+
+    impl From<JsGcsStaticCredentials> for icechunk::config::GcsStaticCredentials {
+        fn from(creds: JsGcsStaticCredentials) -> Self {
+            use icechunk::config::GcsStaticCredentials;
+            match creds {
+                JsGcsStaticCredentials::ServiceAccount(path) => {
+                    GcsStaticCredentials::ServiceAccount(path.into())
+                }
+                JsGcsStaticCredentials::ServiceAccountKey(key) => {
+                    GcsStaticCredentials::ServiceAccountKey(key)
+                }
+                JsGcsStaticCredentials::ApplicationCredentials(path) => {
+                    GcsStaticCredentials::ApplicationCredentials(path.into())
+                }
+                JsGcsStaticCredentials::BearerToken(token) => {
+                    GcsStaticCredentials::BearerToken(
+                        icechunk::config::GcsBearerCredential {
+                            bearer: token,
+                            expires_after: None,
+                        },
+                    )
+                }
+            }
+        }
+    }
+
+    /// GCS credentials
+    #[napi(js_name = "GcsCredentials")]
+    pub enum JsGcsCredentials {
+        Anonymous,
+        FromEnv,
+        Static(JsGcsStaticCredentials),
+    }
+
+    impl From<JsGcsCredentials> for icechunk::config::GcsCredentials {
+        fn from(creds: JsGcsCredentials) -> Self {
+            use icechunk::config::GcsCredentials;
+            match creds {
+                JsGcsCredentials::Anonymous => GcsCredentials::Anonymous,
+                JsGcsCredentials::FromEnv => GcsCredentials::FromEnv,
+                JsGcsCredentials::Static(c) => GcsCredentials::Static(c.into()),
+            }
+        }
+    }
+
+    /// Azure static credentials
+    #[napi(js_name = "AzureStaticCredentials")]
+    pub enum JsAzureStaticCredentials {
+        AccessKey(String),
+        SasToken(String),
+        BearerToken(String),
+    }
+
+    impl From<JsAzureStaticCredentials> for icechunk::config::AzureStaticCredentials {
+        fn from(creds: JsAzureStaticCredentials) -> Self {
+            use icechunk::config::AzureStaticCredentials;
+            match creds {
+                JsAzureStaticCredentials::AccessKey(key) => {
+                    AzureStaticCredentials::AccessKey(key)
+                }
+                JsAzureStaticCredentials::SasToken(token) => {
+                    AzureStaticCredentials::SASToken(token)
+                }
+                JsAzureStaticCredentials::BearerToken(token) => {
+                    AzureStaticCredentials::BearerToken(token)
+                }
+            }
+        }
+    }
+
+    /// Azure credentials
+    #[napi(js_name = "AzureCredentials")]
+    pub enum JsAzureCredentials {
+        FromEnv,
+        Static(JsAzureStaticCredentials),
+    }
+
+    impl From<JsAzureCredentials> for icechunk::config::AzureCredentials {
+        fn from(creds: JsAzureCredentials) -> Self {
+            use icechunk::config::AzureCredentials;
+            match creds {
+                JsAzureCredentials::FromEnv => AzureCredentials::FromEnv,
+                JsAzureCredentials::Static(c) => AzureCredentials::Static(c.into()),
             }
         }
     }
 }
 
-/// S3 options
-#[napi(object, js_name = "S3Options")]
-#[derive(Clone, Debug)]
-pub struct JsS3Options {
-    pub region: Option<String>,
-    pub endpoint_url: Option<String>,
-    pub allow_http: Option<bool>,
-    pub anonymous: Option<bool>,
-    pub force_path_style: Option<bool>,
-    pub network_stream_timeout_seconds: Option<u32>,
-    pub requester_pays: Option<bool>,
-}
-
-impl JsS3Options {
-    #[allow(clippy::too_many_arguments)]
-    pub fn new(
-        region: Option<String>,
-        endpoint_url: Option<String>,
-        allow_http: Option<bool>,
-        anonymous: Option<bool>,
-        force_path_style: Option<bool>,
-        network_stream_timeout_seconds: Option<u32>,
-        requester_pays: Option<bool>,
-    ) -> Self {
-        Self {
-            region,
-            endpoint_url,
-            allow_http,
-            anonymous,
-            force_path_style,
-            network_stream_timeout_seconds,
-            requester_pays,
-        }
-    }
-}
-
-impl From<JsS3Options> for icechunk::config::S3Options {
-    fn from(opts: JsS3Options) -> Self {
-        icechunk::config::S3Options {
-            region: opts.region,
-            endpoint_url: opts.endpoint_url,
-            allow_http: opts.allow_http.unwrap_or(false),
-            anonymous: opts.anonymous.unwrap_or(false),
-            force_path_style: opts.force_path_style.unwrap_or(false),
-            network_stream_timeout_seconds: opts.network_stream_timeout_seconds,
-            requester_pays: opts.requester_pays.unwrap_or(false),
-        }
-    }
-}
-
-/// GCS static credentials
-#[napi(js_name = "GcsStaticCredentials")]
-pub enum JsGcsStaticCredentials {
-    ServiceAccount(String),
-    ServiceAccountKey(String),
-    ApplicationCredentials(String),
-    BearerToken(String),
-}
-
-impl From<JsGcsStaticCredentials> for icechunk::config::GcsStaticCredentials {
-    fn from(creds: JsGcsStaticCredentials) -> Self {
-        use icechunk::config::GcsStaticCredentials;
-        match creds {
-            JsGcsStaticCredentials::ServiceAccount(path) => {
-                GcsStaticCredentials::ServiceAccount(path.into())
-            }
-            JsGcsStaticCredentials::ServiceAccountKey(key) => {
-                GcsStaticCredentials::ServiceAccountKey(key)
-            }
-            JsGcsStaticCredentials::ApplicationCredentials(path) => {
-                GcsStaticCredentials::ApplicationCredentials(path.into())
-            }
-            JsGcsStaticCredentials::BearerToken(token) => {
-                GcsStaticCredentials::BearerToken(icechunk::config::GcsBearerCredential {
-                    bearer: token,
-                    expires_after: None,
-                })
-            }
-        }
-    }
-}
-
-/// GCS credentials
-#[napi(js_name = "GcsCredentials")]
-pub enum JsGcsCredentials {
-    Anonymous,
-    FromEnv,
-    Static(JsGcsStaticCredentials),
-}
-
-impl From<JsGcsCredentials> for icechunk::config::GcsCredentials {
-    fn from(creds: JsGcsCredentials) -> Self {
-        use icechunk::config::GcsCredentials;
-        match creds {
-            JsGcsCredentials::Anonymous => GcsCredentials::Anonymous,
-            JsGcsCredentials::FromEnv => GcsCredentials::FromEnv,
-            JsGcsCredentials::Static(c) => GcsCredentials::Static(c.into()),
-        }
-    }
-}
-
-/// Azure static credentials
-#[napi(js_name = "AzureStaticCredentials")]
-pub enum JsAzureStaticCredentials {
-    AccessKey(String),
-    SasToken(String),
-    BearerToken(String),
-}
-
-impl From<JsAzureStaticCredentials> for icechunk::config::AzureStaticCredentials {
-    fn from(creds: JsAzureStaticCredentials) -> Self {
-        use icechunk::config::AzureStaticCredentials;
-        match creds {
-            JsAzureStaticCredentials::AccessKey(key) => {
-                AzureStaticCredentials::AccessKey(key)
-            }
-            JsAzureStaticCredentials::SasToken(token) => {
-                AzureStaticCredentials::SASToken(token)
-            }
-            JsAzureStaticCredentials::BearerToken(token) => {
-                AzureStaticCredentials::BearerToken(token)
-            }
-        }
-    }
-}
-
-/// Azure credentials
-#[napi(js_name = "AzureCredentials")]
-pub enum JsAzureCredentials {
-    FromEnv,
-    Static(JsAzureStaticCredentials),
-}
-
-impl From<JsAzureCredentials> for icechunk::config::AzureCredentials {
-    fn from(creds: JsAzureCredentials) -> Self {
-        use icechunk::config::AzureCredentials;
-        match creds {
-            JsAzureCredentials::FromEnv => AzureCredentials::FromEnv,
-            JsAzureCredentials::Static(c) => AzureCredentials::Static(c.into()),
-        }
-    }
-}
+#[cfg(not(target_family = "wasm"))]
+pub use native::*;
 
 #[napi]
 impl JsStorage {
@@ -200,6 +214,7 @@ impl JsStorage {
         Ok(JsStorage(storage))
     }
 
+    #[cfg(not(target_family = "wasm"))]
     #[napi(factory)]
     pub async fn new_local_filesystem(path: String) -> napi::Result<JsStorage> {
         let storage =
@@ -209,6 +224,7 @@ impl JsStorage {
         Ok(JsStorage(storage))
     }
 
+    #[cfg(not(target_family = "wasm"))]
     #[napi(factory)]
     pub fn new_s3(
         bucket: String,
@@ -231,6 +247,7 @@ impl JsStorage {
         Ok(JsStorage(storage))
     }
 
+    #[cfg(not(target_family = "wasm"))]
     #[napi(factory)]
     pub fn new_r2(
         bucket: Option<String>,
@@ -255,6 +272,7 @@ impl JsStorage {
         Ok(JsStorage(storage))
     }
 
+    #[cfg(not(target_family = "wasm"))]
     #[napi(factory)]
     pub fn new_tigris(
         bucket: String,
@@ -285,6 +303,7 @@ impl JsStorage {
         Ok(JsStorage(storage))
     }
 
+    #[cfg(not(target_family = "wasm"))]
     #[napi(factory)]
     pub async fn new_s3_object_store(
         bucket: String,
@@ -309,6 +328,7 @@ impl JsStorage {
         Ok(JsStorage(storage))
     }
 
+    #[cfg(not(target_family = "wasm"))]
     #[napi(factory)]
     pub fn new_gcs(
         bucket: String,
@@ -322,6 +342,7 @@ impl JsStorage {
         Ok(JsStorage(storage))
     }
 
+    #[cfg(not(target_family = "wasm"))]
     #[napi(factory)]
     pub async fn new_azure_blob(
         account: String,
@@ -339,6 +360,7 @@ impl JsStorage {
         Ok(JsStorage(storage))
     }
 
+    #[cfg(not(target_family = "wasm"))]
     #[napi(factory)]
     pub fn new_http(
         base_url: String,
