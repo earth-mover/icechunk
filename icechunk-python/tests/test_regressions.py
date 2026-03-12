e22d552a36d946eb4e5f4e9e741aa46e17a889fa
Sebastian Galkin <code@amisdelabc.com>
2026-03-12 16:28:21 -0300

    Fix bug in list_prefix implementation
    Certain paths with non alphanumeric characters were hiding others.
    
    For example:
    
    `foo-bar` would hide `foo/baz`
    
    Data is safe, but Icechunk wasn't able to return those nodes.
    
    Fixes: #1824

--- icechunk-python/tests/test_regressions.py
+++ icechunk-python/tests/test_regressions.py
@@ -202,3 +202,31 @@ async def test_tag_with_open_session(any_spec_version: int | None) -> None:
     async for k in store.list_prefix(""):
         value = await store.get(k, default_buffer_prototype())
         assert value is not None, k
+
+
+async def test_list_missing_array(any_spec_version: int | None) -> None:
+    """This is an issue found by hypothesis"""
+
+    repo = Repository.create(
+        storage=in_memory_storage(),
+    )
+    session = repo.writable_session("main")
+
+    root = zarr.group(store=session.store)
+    group = root.create_group("foo-bar")
+    group = root.create_group("foo")
+    arrays = {"bar", "baz"}
+    for array in arrays:
+        group.create_array(name=array, shape=((2,)), chunks=((1,)), dtype="i4")
+
+    async def check_keys(store: zarr.abc.store.Store) -> None:
+        keys = set([k async for k in store.list_prefix("foo")])
+        assert keys == {f"foo/{a}/zarr.json" for a in arrays} | {"foo/zarr.json"}
+
+        keys = set([k async for k in store.list_dir("foo")])
+        assert keys == arrays | {"zarr.json"}
+
+    await check_keys(session.store)
+    session.commit("commit")
+    session = repo.readonly_session(branch="main")
+    await check_keys(session.store)
