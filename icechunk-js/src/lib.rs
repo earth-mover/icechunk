eba3cf60c15915ab2972d9a69d669fff5cdf4b3d
Matthew Iannucci <mpiannucci@gmail.com>
2026-03-23 15:35:18 -0400

    More tokio shenanigans

--- icechunk-js/src/lib.rs
+++ icechunk-js/src/lib.rs
@@ -6,3 +6,14 @@ mod repository;
 mod session;
 mod storage;
 mod store;
+
+#[cfg(target_family = "wasm")]
+#[napi::module_init]
+fn init() {
+    napi::create_custom_tokio_runtime(
+        tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .expect("Failed to create tokio runtime"),
+    );
+}
