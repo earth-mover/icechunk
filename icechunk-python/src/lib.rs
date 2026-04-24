267345343f03b353dacd2fe9df32d479cf3ce42e
Luiz Irber <luiz@earthmover.io>
2026-04-24 15:32:27 -0300

    don't run coverage on merge queue, tweaks

--- icechunk-python/src/lib.rs
+++ icechunk-python/src/lib.rs
@@ -172,7 +172,6 @@ fn pep440_version() -> String {
     cargo_version.replace("-rc.", "rc").replace("-alpha.", "a").replace("-beta.", "b")
 }
 
-// LCOV_EXCL_START - PyO3 module initialization cannot be directly tested
 /// The icechunk Python module implemented in Rust.
 #[pymodule]
 fn _icechunk_python(py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
@@ -247,4 +246,3 @@ fn _icechunk_python(py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
 
     Ok(())
 }
-// LCOV_EXCL_STOP
